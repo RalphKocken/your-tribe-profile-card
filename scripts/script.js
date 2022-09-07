255b3b9aba66b225c56574ef99619587ebe5e2b2
RalphKocken <106448490+RalphKocken@users.noreply.github.com>
2022-09-07 18:30:43 +0200

    Profile card version 1
    - Replaced index.html in repo.
    - Replaced style.css in repo.
    - Replaced script.css in repo.
    - Made my own profile card, this is version 1

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,41 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+document.addEventListener("click", e => {
+    const isDropdownButton = e.target.matches("[data-dropdown-button]")
+    if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return
+  
+    let currentDropdown
+    if (isDropdownButton) {
+      currentDropdown = e.target.closest("[data-dropdown]")
+      currentDropdown.classList.toggle("active")
+    }
+  
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+      if (dropdown === currentDropdown) return
+      dropdown.classList.remove("active")
+    })
+  })    
 
-function expand () {
-  document.body.classList.toggle('expand')
+function change(){
+    var image = document.getElementById('vakantiefoto');
+    image.src = "styles/assets/vakantiefoto1.jpg"
+  }
+
+
+function change1(){
+    var image = document.getElementById('vakantiefoto');
+    image.src = "styles/assets/alkmaar.jpg"
+  }
+
+function change2(){
+  var image = document.getElementById('vakantiefoto');
+  image.src = "styles/assets/mountainbiken.jpg"
+}
+
+function change3(){
+  var image = document.getElementById('vakantiefoto');
+  image.src = "styles/assets/koken.jpg"
+}
+
+function change4(){
+  var image = document.getElementById('vakantiefoto');
+  image.src = "styles/assets/front-end.jpg"
 }
